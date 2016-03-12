b75ee2aa09165619ad287597c92effced220fba4
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2016-03-11 22:23:01 -0300

    add new feature

--- serve.go
+++ serve.go
@@ -59,7 +59,7 @@ func PostRoot(c web.C, w http.ResponseWriter, r *http.Request) {
           return
       }
 
-    text := fmt.Sprintf("<!channel> *Content Marketing is amazing* :smile:, pro favor compartilhem\n")
+    text := fmt.Sprintf("<!channel> *Content Marketing is amazing* :smile:, *compartilhem*\n<%s|%s>", og.URL, og.Title)
 
     facebook := Attachments{
       "#36a64f",
